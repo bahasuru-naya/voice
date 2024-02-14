2d2020cb3e76b7703b1da4fbd2f2ec9a724b2868
Bahasuru nayanakantha <76860751+bahasuru-naya@users.noreply.github.com>
2024-02-14 14:19:12 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,2 +1,9 @@
 # Initialize the recognizer
 r = sr.Recognizer()
+# Function to convert text to
+# speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
